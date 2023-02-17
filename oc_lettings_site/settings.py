9666b34d0ad954b71227af2d8349d6420e5401eb
B-asile <84376166+B-asile@users.noreply.github.com>
2023-02-17 10:58:13 +0100

    rollback tests conf sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -41,7 +41,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'sentry_sdk.integrations.django.DjangoIntegration().process_exception',
 ]
 
 ROOT_URLCONF = 'oc_lettings_site.urls'
